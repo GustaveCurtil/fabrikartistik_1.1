00723255fe3fb430ef98412265c64973e40feac1
Gustave Curtil <gustave.curtil@tutanota.com>
2025-09-02 03:03:40 +0200

    eigenlijk ben ik klaar met wat ik wilde voor de landingspagina, zonder de animatie

--- js/letter-transformation.js
+++ js/letter-transformation.js
@@ -8,27 +8,55 @@
     span.style.animation = `wobble ${duration} ease-in-out infinite alternate`;
   });
 
+// document.querySelectorAll("a.sticker").forEach(a => {
+//   const spans = a.querySelectorAll("span");
+
+//   spans.forEach(span => {
+//     // Precompute random values for each span
+//     const start = (Math.random() * 30 - 15).toFixed(1) + "deg"; 
+//     const end   = (Math.random() * 30 - 15).toFixed(1) + "deg";
+//     const duration = (Math.random() * 3 + 2).toFixed(1) + "s"; 
+
+//     a.addEventListener("mouseenter", () => {
+//       a.style.scale = '1';
+//       a.style.rotate = Math.random() * 6 - 3 + `deg`;
+//       span.style.setProperty("--start-angle", start);
+//       span.style.setProperty("--end-angle", end);
+//       span.style.animation = `wobble ${duration} ease-in-out infinite alternate`;
+//     });
+
+//     a.addEventListener("mouseleave", () => {
+//       span.style.animation = "none"; 
+//       a.style.scale = '1';
+//       a.style.rotate = (Math.random() * 8 - 4) + `deg`;
+//     });
+//   });
+// });
+
 document.querySelectorAll("a.sticker").forEach(a => {
   const spans = a.querySelectorAll("span");
 
-  spans.forEach(span => {
-    // Precompute random values for each span
-    const start = (Math.random() * 30 - 15).toFixed(1) + "deg"; 
-    const end   = (Math.random() * 30 - 15).toFixed(1) + "deg";
-    const duration = (Math.random() * 3 + 2).toFixed(1) + "s"; 
+  a.addEventListener("mouseenter", () => {
+    a.style.scale = "1";
+    a.style.rotate = (Math.random() * 6 - 3) + "deg";
 
-    a.addEventListener("mouseenter", () => {
-      a.style.scale = '1';
-      a.style.rotate = Math.random() * 6 - 3 + `deg`;
+    spans.forEach(span => {
+      const start = (Math.random() * 30 - 15).toFixed(1) + "deg"; 
+      const end   = (Math.random() * 30 - 15).toFixed(1) + "deg";
+      const duration = (Math.random() * 3 + 2).toFixed(1) + "s"; 
+
       span.style.setProperty("--start-angle", start);
       span.style.setProperty("--end-angle", end);
       span.style.animation = `wobble ${duration} ease-in-out infinite alternate`;
     });
+  });
 
-    a.addEventListener("mouseleave", () => {
-      span.style.animation = "none"; 
-      a.style.scale = '1';
-      a.style.rotate = (Math.random() * 8 - 4) + `deg`;
+  a.addEventListener("mouseleave", () => {
+    a.style.scale = "1";
+    a.style.rotate = (Math.random() * 8 - 4) + "deg";
+
+    spans.forEach(span => {
+      span.style.animation = "none";
     });
   });
 });
